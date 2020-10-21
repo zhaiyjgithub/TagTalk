a9cb474764a92fc5f5a7d499460535944b49def8
zhaiyuanji <chuck.zhaiyj@gmail.com>
2020-10-21 22:55:12 +0800

    1. Added tab bar.

--- index.js
+++ index.js
@@ -5,12 +5,14 @@
 import {Navigation} from 'react-native-navigation';
 import ChatViewController from './src/components/chat/ChatViewController';
 import MessageViewController from './src/components/chat/MessageViewController';
-import {Colors} from './src/utils/styles';
+import {Colors, FontFamily} from './src/utils/styles';
 import RecordVideoViewController from './src/components/chat/RecordVideoViewController';
+import ContactsViewController from './src/components/contacts/ContactsViewController';
 
 Navigation.registerComponent('MessageViewController', () => MessageViewController);
 Navigation.registerComponent('ChatViewController', () => ChatViewController);
 Navigation.registerComponent('RecordVideoViewController', () => RecordVideoViewController);
+Navigation.registerComponent('ContactsViewController', () => ContactsViewController);
 //
 
 Navigation.events().registerAppLaunchedListener(() => {
@@ -36,20 +38,68 @@ Navigation.events().registerAppLaunchedListener(() => {
       title: {
         text: 'Chats'
       }
-    },
+    }
+  }
+
+  ContactsViewController.options = {
+    topBar: {
+      title: {
+        text: 'Contacts'
+      }
+    }
   }
 
   Navigation.setRoot({
     root: {
-      stack: {
+      bottomTabs: {
         children: [
           {
-            component: {
-              name: 'MessageViewController',
-            },
+            stack: {
+              children: [{
+                component: {
+                  name: 'MessageViewController',
+                  passProps: {
+                    text: 'This is tab 1'
+                  }
+                }
+              }],
+              options: {
+                bottomTab: {
+                  text: 'Chats',
+                  icon: require('./src/source/image/tab/universal.png'),
+                  testID: 'finder',
+                  fontFamily: FontFamily.helvetica,
+                  selectedTextColor: Colors.black,
+                  selectedIconColor: Colors.black,
+                }
+              }
+            }
           },
-        ],
+          {
+            stack: {
+              children: [{
+                component: {
+                  name: 'ContactsViewController',
+                  passProps: {
+                    text: 'This is tab 1'
+                  }
+                }
+              }],
+              options: {
+                bottomTab: {
+                  text: 'Contacts',
+                  icon: require('./src/source/image/tab/universal.png'),
+                  testID: 'Universal',
+                  fontFamily: FontFamily.helvetica,
+                  selectedTextColor: Colors.black,
+                  selectedIconColor: Colors.black,
+                },
+              }
+            }
+          }
+        ]
       },
-    },
-  });
+
+    }
+  })
 });
