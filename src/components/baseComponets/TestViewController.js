3834a49671d4886768c8b7bafed658af68512ffa
zhaiyuanji <chuck.zhaiyj@gmail.com>
2020-11-01 21:07:31 +0800

    1. Added Test Controller

--- src/components/baseComponets/TestViewController.js
+++ src/components/baseComponets/TestViewController.js
@@ -2,27 +2,8 @@ import React, {Component} from 'react';
 import {View, SafeAreaView, TouchableOpacity, Text} from 'react-native';
 import Animated, { useSharedValue, useAnimatedStyle } from 'react-native-reanimated';
 
-export default class TestViewController extends Component{
-	constructor(props) {
-		super(props)
-		this.offset = useSharedValue(0);
-	}
-	render() {
-		const animatedStyles = useAnimatedStyle(() => {
-			return {
-				transform: [{ translateX: this.offset.value * 255 }],
-			};
-		});
-		return (
-			<SafeAreaView style={{flex: 1, }}>
-				<Animated.View style={[{width: 100, height: 100, backgroundColor: 'blue'}, animatedStyles]}/>
-
-				<TouchableOpacity onPress={() => {
-					this.offset.value = Math.random()
-				}} style={{marginTop: 100, }}>
-					<Text>{'Click'}</Text>
-				</TouchableOpacity>
-			</SafeAreaView>
-		)
-	}
+export default function TestController(props) {
+	return(
+		<SafeAreaView style={{flex: 1, backgroundColor: 'red'}}></SafeAreaView>
+	)
 }
