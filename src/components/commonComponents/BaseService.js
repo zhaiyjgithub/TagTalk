4e3fa00d7c2b25bb1d9449434444fecd6919d799
zhaiyuanji <chuck.zhaiyj@gmail.com>
2021-01-04 23:47:22 +0800

    Add Service data layer.

--- src/components/commonComponents/BaseService.js
+++ src/components/commonComponents/BaseService.js
@@ -1,8 +1,4 @@
 import {HTTP} from '../../utils/HttpTools';
-import {API_Contacts} from '../../utils/API';
-import {ResponseCode} from '../../utils/Enums';
-import {logger} from '@nozbe/watermelondb/utils/common';
-
 export default class BaseService {
 	constructor() {
 	}
