7cee573c33a5014fd46e35b9ff5d5caf40a3ef1c
zhaiyuanji <chuck.zhaiyj@gmail.com>
2021-04-25 17:00:49 +0800

    Update profile ui

--- src/components/profile/controller/ProfileViewController.js
+++ src/components/profile/controller/ProfileViewController.js
@@ -10,15 +10,18 @@ import FastImage from 'react-native-fast-image';
 import ProfileImageWallService from '../service/ProfileImageWallService';
 
 let data = []
-for (let i = 0; i < 50; i ++) {
+for (let i = 0; i < 56; i ++) {
     data.push(i)
 }
+
+const ImageWalls = [1, 2, 3, 4]
 
 export default class ProfileViewController extends Component{
     constructor(props) {
         super(props);
         this.state = {
             imageWalls: [],
+            imageIndex: 0,
             pageIndex: PageType.pix
         }
 
@@ -52,7 +55,9 @@ export default class ProfileViewController extends Component{
             'Foreign culture', 'Hitchiking', 'Local food', 'More'
         ]
         return (
-            <View style={{width: '100%', flexDirection: 'row', flexWrap: 'wrap'}}>
+            <View style={{flexDirection: 'row', flexWrap: 'wrap', backgroundColor: Colors.white,
+                marginTop: 8
+            }}>
                 {defaultTags.map((item, idx) => {
                     return this.renderTag(item, idx)
                 })}
@@ -62,32 +67,10 @@ export default class ProfileViewController extends Component{
 
     renderTag = (item, idx) => {
         return (
-            <View  key={idx} style={{height: 40, borderRadius: 20, backgroundColor: Colors.blue, justifyContent: 'center',
-                marginTop: 10, marginRight: 10, paddingHorizontal: 16,
+            <View key={idx} style={{height: 24, borderRadius: 10, backgroundColor: Colors.blue, justifyContent: 'center',
+                marginTop: 8, marginRight: 8, paddingHorizontal: 8,
             }}>
                 <Text style={{color: Colors.white, fontSize: 16, }}>{item}</Text>
-            </View>
-        )
-    }
-
-    renderPostImage = (data) => {
-        const {width, height} = Dimensions.get('window')
-        const size = (width - 32)/4.0
-        return data.map((item, idx) => {
-            const {uri} = item
-            return (
-                <TouchableOpacity key={idx} activeOpacity={0.8}>
-                    <Image resizeMode={'stretch'} source={{uri: uri}} style={{width: size, height: size}}/>
-                </TouchableOpacity>
-            )
-        })
-    }
-
-    renderPostImages = () => {
-        const {imageWalls} = this.state
-        return (
-            <View style={{width: '100%', flexDirection: 'row', flexWrap: 'wrap', marginTop: 16,}}>
-                {this.renderPostImage(imageWalls)}
             </View>
         )
     }
@@ -95,14 +78,15 @@ export default class ProfileViewController extends Component{
     renderUserInfoHeader = () => {
         const {Name, Avatar, Bio} = ACCOUNT
         const uri = BaseUrl + API_User.Avatar + '?name=' + 'ride.png'
-        const size = 96
+        const size = 80
         return (
-            <View style={{alignItems: 'center', backgroundColor: Colors.white, marginTop: 100,
-                paddingBottom: 16
-            }}>
+            <View style={{ borderRadius: 8, marginHorizontal: 20, paddingVertical: 16,
+                paddingHorizontal: 16,
+                backgroundColor: Colors.white, marginTop: -24}}>
+                <View style={{alignItems: 'center', flexDirection: 'row'}}>
                     <FastImage
-                        style={{width: size, height: size, borderRadius: size/2.0, backgroundColor: 'red', borderWidth: 6,
-                            borderColor: Colors.white, position: 'absolute', top: -(size/2.0)
+                        style={{width: size, height: size, borderRadius: size/2.0, backgroundColor: 'red', borderWidth: 4,
+                            borderColor: Colors.blue,
                         }}
                         source={{
                             uri:  uri,
@@ -110,91 +94,84 @@ export default class ProfileViewController extends Component{
                         }}
                         resizeMode={FastImage.resizeMode.contain}
                     />
-                <Text numberOfLines={1} style={{width: '100%', fontSize: 24, color: Colors.black, textAlign: 'center',
-                    fontWeight: 'bold', marginTop: size/2.0
-                }}>{'Time cole'}</Text>
-                <Text style={{width: '100%',fontSize: 14, color: Colors.lightGray, marginTop: 8, textAlign: 'center'}}>{'To be or to be, this is a question.'}</Text>
+                    <View style={{marginLeft: 16}}>
+                        <Text numberOfLines={1} style={{fontSize: 24, color: Colors.black, textAlign: 'left',
+                            fontWeight: 'bold'
+                        }}>{'Time cole'}</Text>
+                        <Text style={{fontSize: 14, color: Colors.lightGray, marginTop: 8, textAlign: 'left'}}>{'To be or to be, this is a question.'}</Text>
+                    </View>
+                </View>
+                {this.renderTagList()}
             </View>
+
         )
     }
 
-    renderPageContainerHeaderIcon = (title, idx) => {
-        return (
-            <TouchableOpacity key={idx}>
-                <Text style={{fontSize: 14, color: Colors.black}}>{title}</Text>
-            </TouchableOpacity>
-        )
+    onAnimationEnd = (e) =>{
+        const {width, height} = Dimensions.get('window')
+        const offSetX = e.nativeEvent.contentOffset.x
+        const currentPage = offSetX / (width)
+        this.setState({imageIndex: parseInt(currentPage)})
     }
 
-    renderPageContainerHeader = () => {
-        const data = [PageType.pix, PageType.tags, PageType.timeline]
+    renderImageWall = () => {
         return (
-            <View style={{paddingHorizontal: 16, flexDirection: 'row', alignItems: 'center', backgroundColor: Colors.white,
-                height: 30, marginTop: 16, justifyContent: 'space-between'
-            }}>
-                {data.map((val, idx) => {
-                    return this.renderPageContainerHeaderIcon(val, idx)
-                })}
-            </View>
-        )
-}
-
-    renderPageContainer = () => {
-        return (
-            <ScrollView pagingEnabled={true} horizontal={true} showsHorizontalScrollIndicator={false}>
-                {this.renderImageWallPage()}
-                {this.renderTagsPage()}
-                {this.renderTimelinePage()}
-            </ScrollView>
+          <View style={{width: '100%'}}>
+              <ScrollView
+                  onMomentumScrollEnd={(e) => {
+                      this.onAnimationEnd(e)
+                  }}
+                  style={{height: 196, width: '100%'}} pagingEnabled={true} horizontal={true} showsHorizontalScrollIndicator={false}>
+                  {ImageWalls.map((val, idx) => {
+                      return this.renderImageWallItem(idx)
+                  })}
+              </ScrollView>
+              {this.renderImageWallIndicator()}
+          </View>
         )
     }
 
     renderImageWallItem = (idx) => {
         const {width, height} = Dimensions.get('window')
-        const number = 4
-        const size = (width - 32 - 4*(number - 1))/number
-
         return (
-            <FastImage
-                style={{width: size, height: size, marginBottom: 4}}
-                source={{
-                    uri: `https://picsum.photos/id/${idx + 10}/400/400`,
-                    priority: FastImage.priority.normal,
-                }}
-                resizeMode={FastImage.resizeMode.contain}
-            />
-        )
-    }
-
-    renderImageWallPage = () => {
-        const {width, height} = Dimensions.get('window')
-        return (
-            <View style={{backgroundColor: Colors.white, width: width, flexDirection: 'row',
-                flexWrap: 'wrap', paddingHorizontal: 16, justifyContent: 'space-between'
-            }}>
-                {data.map((val, idx) => {
-                    return this.renderImageWallItem(idx)
-                })}
+            <View style={{width: width}}>
+                <FastImage
+                    style={{width: '100%', height: 196, marginBottom: 4}}
+                    source={{
+                        uri: `https://picsum.photos/id/${idx + 10}/400/400`,
+                        priority: FastImage.priority.normal,
+                    }}
+                    resizeMode={FastImage.resizeMode.cover}
+                />
             </View>
         )
     }
 
-    renderTagsPage = () => {
-        const {width, height} = Dimensions.get('window')
+    renderImageWallIndicator = () => {
+        const {imageIndex} = this.state
         return (
-            <View style={{width: width, flexDirection: 'row', flexWrap: 'wrap', paddingHorizontal: 16}}>
-                {this.renderTagList()}
+            <View style={{flexDirection: 'row', alignItems: 'center',
+                position: 'absolute', top: 8, right: 8,
+            }}>
+                {ImageWalls.map((item, idx) => {
+                    return (
+                        <View style={{width: 8, height: 8, borderRadius: 4,
+                            backgroundColor: (idx === imageIndex) ? Colors.blue : Colors.systemGray,
+                            marginRight: 4
+                        }}/>
+                    )
+                })}
             </View>
         )
     }
 
     renderTimelineImageItem = (idx) => {
         const {width, height} = Dimensions.get('window')
-        const number = 5
-        const size = (width - 40 - 4*(number - 1))/number
+        const number = 4
+        const size = (width - 32 - 4*(number - 1))/number
         return (
             <FastImage
-                style={{width: size, height: size, marginBottom: 4}}
+                style={{width: size, height: size, marginBottom: 4, borderRadius: 4}}
                 source={{
                     uri: `https://picsum.photos/id/${idx + 10}/400/400`,
                     priority: FastImage.priority.normal,
@@ -205,10 +182,10 @@ export default class ProfileViewController extends Component{
     }
 
     renderTimelinePage = () => {
-        const {width, height} = Dimensions.get('window')
         return (
-            <View style={{backgroundColor: Colors.white, width: width, flexDirection: 'row',
-                flexWrap: 'wrap', justifyContent: 'space-between', paddingHorizontal: 16
+            <View style={{width: '100%', flexDirection: 'row',
+                flexWrap: 'wrap', justifyContent: 'space-between', paddingHorizontal: 16,
+                marginTop: 16,
             }}>
                 {data.map((val, idx) => {
                     return this.renderTimelineImageItem(idx)
@@ -217,33 +194,14 @@ export default class ProfileViewController extends Component{
         )
     }
 
-    renderHeader = () => {
-        return (
-            <View style={{width: '100%', height: 100, backgroundColor: Colors.black}}/>
-        )
-    }
-
-    renderItem = ({type}) => {
-
-    }
-
     render() {
         return(
             <ContainerView>
-                <FlatList
-                    style={{flex: 1}}
-                    data={[DateType.user, DateType.post]}
-                    renderItem={this.renderItem}
-                    keyExtractor={(item, index) => {
-                        return 'key' + item.key + index
-                    }}
-                    ListHeaderComponent={this.renderHeader}
-                />
-                {/*<ScrollView style={{flex: 1, backgroundColor: Colors.systemGray,}}>*/}
-                {/*    {this.renderUserInfoHeader()}*/}
-                {/*    {this.renderPageContainerHeader()}*/}
-                {/*    {this.renderPageContainer()}*/}
-                {/*</ScrollView>*/}
+                <ScrollView style={{flex: 1, backgroundColor: Colors.systemGray,}}>
+                    {this.renderImageWall()}
+                    {this.renderUserInfoHeader()}
+                    {this.renderTimelinePage()}
+                </ScrollView>
             </ContainerView>
         )
     }
