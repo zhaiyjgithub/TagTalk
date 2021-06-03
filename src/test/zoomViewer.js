4df810e6b1c208a367781e5668ede84239eac8bb
zhaiyuanji <chuck.zhaiyj@gmail.com>
2021-06-03 16:33:36 +0800

    Add zoom viewer.

--- src/test/zoomViewer.js
+++ src/test/zoomViewer.js
@@ -1,21 +1,14 @@
-import React, { useState, useEffect, } from "react"
-import {SafeAreaView, TouchableOpacity, View, Image, Dimensions} from 'react-native'
-import {Colors} from '../utils/styles';
+import React from 'react';
+import {Dimensions} from 'react-native';
 import Animated, {
-	useSharedValue,
-	withTiming,
-	useAnimatedStyle,
-	Easing,
 	useAnimatedGestureHandler,
+	useAnimatedStyle,
+	useSharedValue,
 	withSpring,
-	interpolate,
-	concat,
-	withSequence, exp,
-	multiply
+	withTiming,
 } from 'react-native-reanimated';
-import {PanGestureHandler, State, PinchGestureHandler} from 'react-native-gesture-handler'
+import {PanGestureHandler, PinchGestureHandler} from 'react-native-gesture-handler';
 import ContainerView from '../baseComponents/ContainerView';
-const {width} = Dimensions.get('window')
 
 const ZoomViewer = (props) => {
 	const {width, height} = Dimensions.get('window')
@@ -32,20 +25,38 @@ const ZoomViewer = (props) => {
 			ctx.startY = translation.y.value
 		},
 		onActive: (event, ctx) => {
-			if (imageScale.value > 1.0) {
-
-				translation.x.value = ctx.startX + event.translationX //(event.translationX > maxTranslationX ? maxTranslationX : (event.translationX))
-				translation.y.value = ctx.startY + event.translationY
-				console.log('on active translationX: ', translation.x.value)
-			}
+			translation.x.value = ctx.startX + event.translationX //(event.translationX > maxTranslationX ? maxTranslationX : (event.translationX))
+			translation.y.value = ctx.startY + event.translationY
 		},
 		onEnd: (event, ctx) => {
-			console.log('on end translationX: ', translation.x.value)
+			const translationX = translation.x.value
+			const translationY = translation.y.value
+
 			const maxTranslationX = (((imageScale.value - 1.0)*width)/2.0)
-			console.log('max translationX: ', maxTranslationX)
-			if (translation.x.value*imageScale.value > maxTranslationX) {
-				translation.x.value = withSpring(maxTranslationX/imageScale.value)
+			const maxTranslationY = (((imageScale.value - 1.0)*width)/2.0)
+
+			//For X-axis
+			if (translationX > 0) { // 右滑动
+				if (translationX*imageScale.value > maxTranslationX) {
+					translation.x.value = withSpring(maxTranslationX/imageScale.value)
+				}
+			}else {// 左滑动
+				if (Math.abs(translationX)*imageScale.value > maxTranslationX) {
+					translation.x.value = withSpring(-maxTranslationX/imageScale.value)
+				}
 			}
+
+			//For Y-axis
+			if (translationY > 0) { // 右滑动
+				if (translationY*imageScale.value > maxTranslationY) {
+					translation.y.value = withSpring(maxTranslationY/imageScale.value)
+				}
+			}else {// 左滑动
+				if (Math.abs(translationY)*imageScale.value > maxTranslationY) {
+					translation.y.value = withSpring(-maxTranslationY/imageScale.value)
+				}
+			}
+
 		}
 	})
 
@@ -55,16 +66,16 @@ const ZoomViewer = (props) => {
 		onActive: (event, ctx) => {
 			const {scale} = event
 			const newValue = imageScale.value*scale
-			if (newValue >= 0.7 && newValue < 2.5) {
-				imageScale.value = newValue
+			if (newValue >= 0.7 && newValue < 3.5) {
+				imageScale.value = withTiming(Math.sqrt(newValue))
 			}
 		},
 		onEnd: () => {
-			if (imageScale.value < 1.0) {
+			if (imageScale.value <= 1.0) {
 				imageScale.value = withTiming(1.0)
+				translation.x.value = withTiming(0)
+				translation.y.value = withTiming(0)
 			}
-
-			console.log('image scale value: ', imageScale.value)
 		}
 	})
 
@@ -76,7 +87,6 @@ const ZoomViewer = (props) => {
 			]}
 	})
 
-
 	const size = width
 	return (
 		<ContainerView style={{justifyContent: 'center', alignItems: 'center'}}>
@@ -87,7 +97,6 @@ const ZoomViewer = (props) => {
 					</PinchGestureHandler>
 				</Animated.View>
 			</PanGestureHandler>
-
 		</ContainerView>
 	)
 }
